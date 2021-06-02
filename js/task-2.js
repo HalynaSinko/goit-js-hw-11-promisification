a5a397fee387046bac3368f997d14cc7049ad0d6
Halyna Sinko <galinasinko14@gmail.com>
2021-06-02 10:39:58 +0300

    Добавляет второй способ решения.

--- js/task-2.js
+++ js/task-2.js
@@ -23,7 +23,36 @@ toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
 
 
-// Было
+
+
+// 2-й способ решения используя статический метод Promise.resolve
+
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName) => {
+//   return Promise.resolve(
+//     allUsers.map(user =>
+//       user.name === userName ? { ...user, active: !user.active } : user,
+//     ),
+//   );
+// };
+
+// const logger = result => console.table(result);
+
+// /*
+//  * Должно работать так
+//  */
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
+
+
+
+// Исходное задание (используя callback-функцию)
 // const toggleUserState = (allUsers, userName, callback) => {
 //   const updatedUsers = allUsers.map(user =>
 //     user.name === userName ? { ...user, active: !user.active } : user,
