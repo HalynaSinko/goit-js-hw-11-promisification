d889108521c692f77e21e74389a9883620da0130
Halyna Sinko <galinasinko14@gmail.com>
2021-06-03 18:47:15 +0300

    Замена массива на объект при передаче параметров функции.

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = ({id}) => {
     const delay = randomIntegerFromInterval(200, 500);
    
     return new Promise((resolve, reject) => {
@@ -11,19 +11,19 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve([transaction.id, delay]);
+        resolve({id, time: delay});
       } else {
-        reject(transaction.id);
+        reject({id});
       }
     }, delay);
   });
 };
 
-const logSuccess = ([id, time]) => {
+const logSuccess = ({id, time}) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
 
-const logError = id => {
+const logError = ({id}) => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 // /*
